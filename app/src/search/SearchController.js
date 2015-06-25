e9462c9971b03d391c7c8b351929c7629da27864
Timo Stollenwerk <tisto@plone.org>
2015-06-25 18:22:42 +0200

    Remove unused code.

--- app/src/search/SearchController.js
+++ app/src/search/SearchController.js
@@ -26,6 +26,7 @@
         },
       }
     ];
+
     self.simulateQuery = false;
     self.isDisabled    = false;
     // list of `state` value/display objects
@@ -84,93 +85,6 @@
         return (state.value.indexOf(lowercaseQuery) === 0);
       };
     }
-  }
-
-
-  /**
-   * Main Controller for the Angular Material Starter App
-   * @param $scope
-   * @param $mdSidenav
-   * @param avatarsService
-   * @constructor
-   */
-  function UserController( userService, $mdSidenav, $mdBottomSheet, $log, $q) {
-    var self = this;
-
-    self.selected     = null;
-    self.search        = [ ];
-    self.selectUser   = selectUser;
-    self.toggleList   = toggleSearchList;
-    self.showContactOptions  = showContactOptions;
-
-    // Load all registered search
-
-    userService
-          .loadAllSearch()
-          .then( function( search ) {
-            self.search    = [].concat(search);
-            self.selected = search[0];
-          });
-
-    // *********************************
-    // Internal methods
-    // *********************************
-
-    /**
-     * First hide the bottomsheet IF visible, then
-     * hide or Show the 'left' sideNav area
-     */
-    function toggleSearchList() {
-      var pending = $mdBottomSheet.hide() || $q.when(true);
-
-      pending.then(function(){
-        $mdSidenav('left').toggle();
-      });
-    }
-
-    /**
-     * Select the current avatars
-     * @param menuId
-     */
-    function selectUser ( user ) {
-      self.selected = angular.isNumber(user) ? $scope.search[user] : user;
-      self.toggleList();
-    }
-
-    /**
-     * Show the bottom sheet
-     */
-    function showContactOptions($event) {
-        var user = self.selected;
-
-        return $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: './src/search/view/contactSheet.html',
-          controller: [ '$mdBottomSheet', ContactPanelController],
-          controllerAs: "cp",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
-        });
-
-        /**
-         * Bottom Sheet controller for the Avatar Actions
-         */
-        function ContactPanelController( $mdBottomSheet ) {
-          this.user = user;
-          this.actions = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.submitContact = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
-    }
-
   }
 
 })();
