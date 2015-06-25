cb761d8b20bf6d6d5c1b18a8ff4cff2ebf94865b
Timo Stollenwerk <tisto@plone.org>
2015-06-25 18:06:26 +0200

    Initial import.

---
+++ app/src/search/SearchController.js
@@ -0,0 +1,154 @@
+(function(){
+
+  angular.module('search').controller('SearchCtrl', SearchCtrl);
+
+  function SearchCtrl ($timeout, $q, $log) {
+    var self = this;
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+    // list of `state` value/display objects
+    self.states        = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    }
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+              Wisconsin, Wyoming';
+      return allStates.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+    }
+  }
+
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function UserController( userService, $mdSidenav, $mdBottomSheet, $log, $q) {
+    var self = this;
+
+    self.selected     = null;
+    self.search        = [ ];
+    self.selectUser   = selectUser;
+    self.toggleList   = toggleSearchList;
+    self.showContactOptions  = showContactOptions;
+
+    // Load all registered search
+
+    userService
+          .loadAllSearch()
+          .then( function( search ) {
+            self.search    = [].concat(search);
+            self.selected = search[0];
+          });
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * First hide the bottomsheet IF visible, then
+     * hide or Show the 'left' sideNav area
+     */
+    function toggleSearchList() {
+      var pending = $mdBottomSheet.hide() || $q.when(true);
+
+      pending.then(function(){
+        $mdSidenav('left').toggle();
+      });
+    }
+
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectUser ( user ) {
+      self.selected = angular.isNumber(user) ? $scope.search[user] : user;
+      self.toggleList();
+    }
+
+    /**
+     * Show the bottom sheet
+     */
+    function showContactOptions($event) {
+        var user = self.selected;
+
+        return $mdBottomSheet.show({
+          parent: angular.element(document.getElementById('content')),
+          templateUrl: './src/search/view/contactSheet.html',
+          controller: [ '$mdBottomSheet', ContactPanelController],
+          controllerAs: "cp",
+          bindToController : true,
+          targetEvent: $event
+        }).then(function(clickedItem) {
+          clickedItem && $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * Bottom Sheet controller for the Avatar Actions
+         */
+        function ContactPanelController( $mdBottomSheet ) {
+          this.user = user;
+          this.actions = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+          ];
+          this.submitContact = function(action) {
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+  }
+
+})();
